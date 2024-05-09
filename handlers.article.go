2633128fd8190b361c973d9a3e578aa41bec37a2
cybervasyan <xzkto258@gmail.com>
2024-05-09 14:10:06 +0300

    finish

--- handlers.article.go
+++ handlers.article.go
@@ -8,14 +8,9 @@ import (
 
 func showIndex(c *gin.Context) {
 	articles := getAllArticles()
-	c.HTML(
-		http.StatusOK,
-		"index.html",
-		gin.H{
-			"title":   "Home Page",
-			"payload": articles,
-		},
-	)
+	render(c, gin.H{
+		"title":   "Home Page",
+		"payload": articles}, "index.html")
 }
 
 func getArticle(c *gin.Context) {
@@ -37,4 +32,17 @@ func getArticle(c *gin.Context) {
 	} else {
 		c.AbortWithStatus(http.StatusNotFound)
 	}
+}
+
+func render(c *gin.Context, data gin.H, templateName string) {
+
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		c.JSON(http.StatusOK, data["payload"])
+	case "application/xml":
+		c.XML(http.StatusOK, data["payload"])
+	default:
+		c.HTML(http.StatusOK, templateName, data)
+	}
+
 }
